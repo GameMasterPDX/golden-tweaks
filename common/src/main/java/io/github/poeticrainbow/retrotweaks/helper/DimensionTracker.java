b458d719d310eb65a6396dfd3942780ff59b987a
PoeticRainbow <jarrett.bayer@outlook.com>
2026-03-26 21:41:28 -0400

    add dimension tracker for level loading screen tweak

---
+++ common/src/main/java/io/github/poeticrainbow/retrotweaks/helper/DimensionTracker.java
@@ -0,0 +1,28 @@
+package io.github.poeticrainbow.retrotweaks.helper;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.level.dimension.BuiltinDimensionTypes;
+import org.jetbrains.annotations.Nullable;
+
+public class DimensionTracker {
+    @Nullable
+    private static Identifier previousDimension;
+    @Nullable
+    private static Identifier currentDimension;
+
+    public static Identifier previousDimension() {
+        return previousDimension == null ? BuiltinDimensionTypes.OVERWORLD.identifier() : previousDimension;
+    }
+
+    public static Component previousDimensionTranslation() {
+        var id = previousDimension();
+        return Component.translatable("dimension." + id.getNamespace() + "." + id.getPath());
+    }
+
+    public static void updateDimension(@Nullable Identifier newDimension) {
+        // prev <- current <- new
+        DimensionTracker.previousDimension = currentDimension;
+        DimensionTracker.currentDimension = newDimension;
+    }
+}
