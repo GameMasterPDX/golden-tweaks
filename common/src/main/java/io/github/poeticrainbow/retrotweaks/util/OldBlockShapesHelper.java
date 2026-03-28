e38dcb7b63ef47cfe16e0936493962be56244124
PoeticRainbow <jarrett.bayer@outlook.com>
2026-03-28 15:38:15 -0400

    completely overhaul old hitbox shapes tweak

--- common/src/main/java/io/github/poeticrainbow/retrotweaks/util/OldBlockShapesHelper.java
+++ common/src/main/java/io/github/poeticrainbow/retrotweaks/util/OldBlockShapesHelper.java
@@ -1,7 +1,13 @@
 package io.github.poeticrainbow.retrotweaks.util;
 
 import io.github.poeticrainbow.retrotweaks.tweak.Tweaks;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.AbstractChestBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.FenceBlock;
+import net.minecraft.world.level.block.FenceGateBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
@@ -12,6 +18,46 @@ public class OldBlockShapesHelper {
 
     public static boolean shouldOverrideBlockShapes() {
         return Tweaks.OLD_HITBOX_SHAPES.get();
+    }
+
+    /**
+     * We cannot directly call {@link BlockBehaviour.BlockStateBase#is(TagKey)}
+     * due to tags not being bound on startup.
+     *
+     * @author PoeticRainbow
+     */
+    public static boolean is(BlockBehaviour.BlockStateBase state, TagKey<Block> blockTagKey) {
+        try {
+            return state.is(blockTagKey);
+        } catch (IllegalStateException e) {
+            return false;
+        }
+    }
+
+    public static Optional<VoxelShape> getCollisionShapeForState(BlockBehaviour.BlockStateBase state) {
+        if (state.getBlock() instanceof AbstractChestBlock<?>) {
+            return getFullBlockShape();
+        }
+        if (state.getBlock() instanceof FenceBlock || is(state, BlockTags.FENCES)) {
+            return getFenceShape();
+        }
+        if (state.getBlock() instanceof FenceGateBlock || is(state, BlockTags.FENCE_GATES)) {
+            if (state.hasProperty(FenceGateBlock.OPEN) && state.getValue(FenceGateBlock.OPEN)) {
+                return getEmptyShape();
+            }
+            return getFenceShape();
+        }
+        return Optional.empty();
+    }
+
+    public static Optional<VoxelShape> getOutlineShapeForState(BlockBehaviour.BlockStateBase state) {
+        if (state.getBlock() instanceof AbstractChestBlock<?>) {
+            return getFullBlockShape();
+        }
+        if (is(state, BlockTags.FENCES) || is(state, BlockTags.FENCE_GATES) || is(state, BlockTags.STAIRS)) {
+            return getFullBlockShape();
+        }
+        return Optional.empty();
     }
 
     public static Optional<VoxelShape> getFullBlockShape() {
