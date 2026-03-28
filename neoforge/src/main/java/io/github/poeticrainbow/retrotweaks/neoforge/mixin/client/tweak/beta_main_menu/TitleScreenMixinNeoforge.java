94fb6466bbc0de7102b9196e63dc06e3f4026dbb
PoeticRainbow <jarrett.bayer@outlook.com>
2026-03-28 17:31:36 -0400

    implement old main menu fully on neoforge

--- neoforge/src/main/java/io/github/poeticrainbow/retrotweaks/neoforge/mixin/client/tweak/beta_main_menu/TitleScreenMixinNeoforge.java
+++
@@ -1,9 +0,0 @@
-package io.github.poeticrainbow.retrotweaks.neoforge.mixin.client.tweak.beta_main_menu;
-
-import net.minecraft.client.gui.screens.TitleScreen;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(TitleScreen.class)
-public class TitleScreenMixinNeoforge {
-    // TODO: implement beta text in neoforge since they fuck up the version stuff
-}
