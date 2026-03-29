a72e1d517d215536595228ebbe3f7eb3d52f02d3
PoeticRainbow <jarrett.bayer@outlook.com>
2026-03-29 17:25:30 -0400

    fix instant consumption tweak

--- common/src/main/java/io/github/poeticrainbow/retrotweaks/mixin/common/tweak/instant_consumption/ConsumableMixin.java
+++ common/src/main/java/io/github/poeticrainbow/retrotweaks/mixin/common/tweak/instant_consumption/ConsumableMixin.java
@@ -2,14 +2,10 @@ package io.github.poeticrainbow.retrotweaks.mixin.common.tweak.instant_consumpti
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.mojang.datafixers.util.Either;
 import io.github.poeticrainbow.retrotweaks.tweak.Tweaks;
 import net.minecraft.core.Holder;
-import net.minecraft.core.HolderOwner;
-import net.minecraft.resources.Identifier;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.tags.TagKey;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -17,6 +13,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.Consumable;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.gameevent.GameEvent;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -24,19 +21,15 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.Optional;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
-
 @Mixin(Consumable.class)
 public abstract class ConsumableMixin {
-    @Shadow public abstract boolean canConsume(LivingEntity livingEntity, ItemStack itemStack);
-    @Shadow public abstract ItemStack onConsume(Level level, LivingEntity livingEntity, ItemStack itemStack);
-
-    @Mutable
     @Shadow
-    @Final
-    private Holder<SoundEvent> sound;
+    public abstract boolean canConsume(LivingEntity livingEntity, ItemStack itemStack);
+
+    @Shadow
+    public abstract ItemStack onConsume(Level level, LivingEntity livingEntity, ItemStack itemStack);
+
+    @Mutable @Shadow @Final private Holder<SoundEvent> sound;
 
     @WrapMethod(method = "startConsuming")
     public InteractionResult retrotweaks$start_consuming(LivingEntity livingEntity, ItemStack itemStack, InteractionHand interactionHand, Operation<InteractionResult> original) {
@@ -54,86 +47,24 @@ public abstract class ConsumableMixin {
 
     @WrapMethod(method = "emitParticlesAndSounds")
     public void retrotweaks$dont_emit_particles_and_sounds(RandomSource randomSource, LivingEntity livingEntity, ItemStack itemStack, int i, Operation<Void> original) {
-        if (Tweaks.INSTANT_CONSUMPTION.get()) {
-            //do nothing
-        } else {
+        if (!Tweaks.INSTANT_CONSUMPTION.get()) {
             original.call(randomSource, livingEntity, itemStack, i);
         }
+        //do nothing
     }
 
     @Redirect(method = "onConsume", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;gameEvent(Lnet/minecraft/core/Holder;)V"))
-    public void retrotweaks$use_no_anim(LivingEntity instance, Holder holder) {
-        if (Tweaks.INSTANT_CONSUMPTION.get()) {
-            // do nothing i think
-        } else {
+    public void retrotweaks$use_no_anim(LivingEntity instance, Holder<GameEvent> holder) {
+        if (!Tweaks.INSTANT_CONSUMPTION.get()) {
             instance.gameEvent(holder);
         }
+        // do nothing i think
     }
 
     @WrapMethod(method = "sound")
     public Holder<SoundEvent> sound(Operation<Holder<SoundEvent>> original) {
         if (Tweaks.INSTANT_CONSUMPTION.get()) {
-            this.sound = new Holder<SoundEvent>() {
-                @Override
-                public SoundEvent value() {
-                    return null;
-                }
-
-                @Override
-                public boolean isBound() {
-                    return false;
-                }
-
-                @Override
-                public boolean is(Identifier identifier) {
-                    return false;
-                }
-
-                @Override
-                public boolean is(ResourceKey<SoundEvent> resourceKey) {
-                    return false;
-                }
-
-                @Override
-                public boolean is(Predicate<ResourceKey<SoundEvent>> predicate) {
-                    return false;
-                }
-
-                @Override
-                public boolean is(TagKey<SoundEvent> tagKey) {
-                    return false;
-                }
-
-                @Override
-                public boolean is(Holder<SoundEvent> holder) {
-                    return false;
-                }
-
-                @Override
-                public Stream<TagKey<SoundEvent>> tags() {
-                    return Stream.empty();
-                }
-
-                @Override
-                public Either<ResourceKey<SoundEvent>, SoundEvent> unwrap() {
-                    return null;
-                }
-
-                @Override
-                public Optional<ResourceKey<SoundEvent>> unwrapKey() {
-                    return Optional.empty();
-                }
-
-                @Override
-                public Kind kind() {
-                    return null;
-                }
-
-                @Override
-                public boolean canSerializeIn(HolderOwner<SoundEvent> holderOwner) {
-                    return false;
-                }
-            };
+            this.sound = Holder.direct(SoundEvents.EMPTY);
             return this.sound;
         } else {
             return original.call();
